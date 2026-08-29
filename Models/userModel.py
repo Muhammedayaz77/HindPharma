c7a2b510a1809eb1e517392c4dd1f81c03730551
Mohammed Ayaz <ayaz.job2010@gmail.com>
2026-08-30 00:25:14 +0530

    Add login user database model

---
+++ Models/userModel.py
@@ -0,0 +1,10 @@
+from .database import get_connection
+
+class UserModel:
+    @staticmethod
+    def find_by_username(username: str):
+        with get_connection() as connection:
+            return connection.execute(
+                "SELECT id, username, password_hash, is_active FROM users WHERE username = %s",
+                (username,),
+            ).fetchone()
