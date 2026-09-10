d5e0c3b5d5a9f4a86e154980c7702af26f31525a
Mohammed Ayaz <ayaz.job2010@gmail.com>
2026-09-10 18:38:23 +0530

    Configure iOS API endpoint

--- ios/HindPharmaApp/Helper/AppConfig.swift
+++ ios/HindPharmaApp/Helper/AppConfig.swift
@@ -2,4 +2,6 @@ import Foundation
 
 enum AppConfig {
     static let appName = "Hind Pharma"
+    // iOS Simulator can reach a backend running on the developer machine via localhost.
+    static let apiBaseURL = URL(string: "http://127.0.0.1:8000/api")!
 }
