3bed9150cb589c573450b6f2fe3f9819611ce5c3
Mohammed Ayaz <ayaz.job2010@gmail.com>
2026-09-10 18:05:20 +0530

    Add Android model layer scaffold

---
+++ android/app/src/main/java/com/hindtechgroup/hindpharma/Models/AppModels.kt
@@ -0,0 +1,6 @@
+package com.hindtechgroup.hindpharma.Models
+
+/**
+ * Shared domain models will be added here as each web feature is ported.
+ * Models remain Android-specific and are not shared with iOS or web code.
+ */
